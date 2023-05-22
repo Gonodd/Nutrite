e8e0d72cda69d8bcf95b60b9f9a89ec4c0e71555
Gonodd <oddeni.gonzalo@gmail.com>
2023-05-22 11:46:10 -0300

    Parcial delivey

---
+++ pedidos/src/Data Base Fake/week.js
@@ -0,0 +1,32 @@
+const week={
+    monday:{
+        clasic: "Pastas con salteado de verduras y cubos de carne vacuna",
+        noFlour:"Hamburguesa de carne vacuna + ensalada tibia de remolacha,papa ,brócoli,zanahoria y huevo", 
+        vegetarian:"Pastel de papas (con soja texturizada)",
+        diet:"Tarteletas de atún",
+    },
+    tuesday:{
+        clasic: "Pollo al horno +ensalada de garbanzo, zanahoria, remolacha y huevo",
+        noFlour:"Pimientos rellenos(arroz yamani carne,cebolla,tomate,pimiento)",
+        vegetarian:"3 empanadas de cebolla,huevo,queso 3 empanadas de tomate, albahaca y queso",
+        diet:"Cazuela de pollo y verduras al vino blanco"
+    },
+    wednesday:{
+        clasic: "Canelones de choclo",
+        noFlour:"Brochettes mixtos (cerdo,vaca,pollo, vegetales)+ ensalada de tomate y hojas verdes",
+        vegetarian:"2 Fajitas",
+        diet:"Hamburguesa de arroz y verduras + cubos zapallo y gajos de batatas al horno"
+    },
+    thursday:{
+        clasic: "Medallón de pescado + papas a la crema",
+        noFlour:"Filet de merluza al Roquefort con puré",
+        vegetarian:"Croquetas de calabacín, garbanzo y queso con ensalada de lechuga, zanahoria, tomate y huevo",
+        diet:"Rebozados de Zucchini rellenos de jamón y queso + ensalada de repollo blancoy tomate"
+    },
+    friday:{
+        clasic: "Bondiola de cerdo a la naranja + arroz",
+        noFlour:"1 Costeleta vacuna + rodajas de calabacín saborizados y ensal de legumbres conpimientos y cebollita",
+        vegetarian:"Lasagna de Zucchini (relleno de queso, tomate y acelga)",
+        diet:"Vittel tonne de lengua vacuna + Ensalada tibia de papa, huevo y perejil"
+    }
+}
